47754a704618dc2296b858f04b9f6ba051ce2348
David Simmen <david.simmen@bluewin.ch>
2018-02-16 13:59:34 +0100

    fix

--- static_content/js/initwaitnext.js
+++ static_content/js/initwaitnext.js
@@ -3,6 +3,7 @@ define([], function () {
 
     function wait_for_pid(){
         $.getJSON( "/pid/"+pid+"/", function( data ) {
+            alert(data);
             var result = JSON.parse(data);
             if(result.status!="zombie"){
                 setTimeout(wait_for_pid,1000) ;
