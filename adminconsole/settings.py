49cc716dcb5c2e33661cd72019ca8633621cdbf3
David Simmen <david.simmen@bluewin.ch>
2018-11-05 16:29:59 +0100

    sl/tls stuff for emails

--- adminconsole/settings.py
+++ adminconsole/settings.py
@@ -130,18 +130,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_content"),
 ]
 
-QA_SETTINGS = {
-    'qa_messages': True,
-    'qa_description_optional': False,
-    'reputation': {
-        'CREATE_QUESTION': 0,
-        'CREATE_ANSWER': 0,
-        'CREATE_ANSWER_COMMENT': 0,
-        'CREATE_QUESTION_COMMENT': 0,
-        'ACCEPT_ANSWER': 0,
-        'UPVOTE_QUESTION': 0,
-        'UPVOTE_ANSWER': 0,
-        'DOWNVOTE_QUESTION': 0,
-        'DOWNVOTE_ANSWER': 0,
-    }
-}
