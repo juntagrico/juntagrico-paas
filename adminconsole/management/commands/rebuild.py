7738c928942eb74b2553a76bdfa7b3f22140c445
root <root@31991.hostserv.eu>
2024-06-17 07:48:06 +0200

    commands for new docker file

---
+++ adminconsole/management/commands/rebuild.py
@@ -0,0 +1,20 @@
+import docker
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('app_name', nargs=1)
+        parser.add_argument('python_version', nargs='?', default='3.9')
+
+    # entry point used by manage.py
+    def handle(self, *args, **options):
+        name = options['app_name'][0]
+        python_version = options['python_version']
+        dir = '/var/django/projects/'+name
+
+        client = docker.from_env()
+
+        result = client.images.build(path=dir, tag=name+':latest', buildargs={'pythonversion':python_version})
+        print(*result[1], sep="\n")
---
+++ adminconsole/management/commands/restart.py
@@ -0,0 +1,48 @@
+import docker
+
+from django.core.management.base import BaseCommand
+
+from adminconsole.models import App
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+
+        parser.add_argument('app_name', nargs=1)
+        parser.add_argument('port', nargs='?')
+
+    # entry point used by manage.py
+    def handle(self, *args, **options):
+        name = options['app_name'][0]
+        port = options['port'] or App.objects.get(name=name).port
+        dir = '/var/django/projects/'+name
+
+        with open(dir+'/build/'+name+'.env') as f:
+            env = f.readlines()
+        env = [x.strip() for x in env]
+        env = [x for x in env if x]
+
+        client = docker.from_env()
+
+        try:
+            container = client.containers.get(name)
+            container.stop()
+            container.remove()
+        except:
+            print('container not found or other error')
+
+        container = client.containers.run(image=name+':latest',
+                                       detach=True,
+                                       environment=env,
+                                       name=name,
+                                       ports={'80':port},
+                                       extra_hosts={"host.docker.internal":"172.17.0.1"},
+                                       restart_policy={'Name': 'always'},
+                                       volumes={dir+'/code': {'bind': '/code/',
+                                                           'mode': 'rw'},
+                                                dir+'/static': {'bind': '/code/static/',
+                                                             'mode': 'rw'},
+                                                dir+'/media': {'bind': '/code/media/',
+                                                            'mode': 'rw'},
+                                               }
+                                        )
+        print(container.status)
