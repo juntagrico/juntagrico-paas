4923b4b82ba08337efd4cbde0322d1ae390c2881
pascalfree <pascal.free@ihdg.ch>
2024-12-01 18:55:16 +0100

    improve logs

--- adminconsole/management/commands/rebuild_docker.py
+++ adminconsole/management/commands/rebuild_docker.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import docker
 import subprocess
 
@@ -18,29 +20,39 @@ class Command(BaseCommand):
 
         container = client.containers.get(name)
 
+        print('Fetch latest code')
         subprocess.run(['git', 'fetch'], cwd=cdir)
         subprocess.run(['git', 'reset', '--hard', '@{u}'], cwd=cdir)
 
+        print('Install Requirements')
         cmd = 'pip install --upgrade -r requirements.txt'
         result = container.exec_run(cmd)
         print(result[1])
 
         if result[0] == 0:  # only continue if pip install succeeded
+            print('Commit to Docker Container')
             result = container.commit(repository=name, tag='latest')
             print(result)
 
+            print('Restart Docker Container')
+            now = datetime.now()
             container.restart()
-            print(container.logs())
+            print(container.logs(since=now))
 
+            print('Django Migrate')
             cmd = ['python', '-m', 'manage', 'migrate']
             result = container.exec_run(cmd)
             print(result[1])
+
+            print('Django Collectstatic')
             cmd = ['python', '-m', 'manage', 'collectstatic', '--noinput', '-c']
             result = container.exec_run(cmd)
             print(result[1])
 
+            print('Restart Docker Container again')
+            now = datetime.now()
             container.restart()
-            print(container.logs())
+            print(container.logs(since=now))
         else:
+            print('ERROR!')
             print('pip install failed! Fix your requirements.txt and redeploy. Do not restart the instance.')
-
