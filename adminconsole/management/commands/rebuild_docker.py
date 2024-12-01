01e359213496139c345b081c8ec977a088b925f2
pascalfree <pascal.free@ihdg.ch>
2024-12-01 19:24:52 +0100

    improve logs

--- adminconsole/management/commands/rebuild_docker.py
+++ adminconsole/management/commands/rebuild_docker.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from time import sleep
 
 import docker
 import subprocess
@@ -35,9 +36,7 @@ class Command(BaseCommand):
             print(result)
 
             print('Restart Docker Container')
-            now = datetime.now()
-            container.restart()
-            print(container.logs(since=now))
+            restart(container)
 
             print('Django Migrate')
             cmd = ['python', '-m', 'manage', 'migrate']
@@ -50,9 +49,24 @@ class Command(BaseCommand):
             print(result[1])
 
             print('Restart Docker Container again')
-            now = datetime.now()
-            container.restart()
-            print(container.logs(since=now))
+            restart(container)
         else:
             print('ERROR!')
             print('pip install failed! Fix your requirements.txt and redeploy. Do not restart the instance.')
+
+
+def restart(container, timeout=20):
+    """ restarts docker container and waits until it is running, then prints the log of the restart
+    :param container: a docker container
+    :param timeout: timeout in seconds
+    """
+    now = datetime.now()
+    container.restart()
+    elapsed_time = 0
+    interval = 1
+    while container.status != 'running' and elapsed_time < timeout:
+        sleep(interval)
+        elapsed_time += interval
+        container.reload()
+        continue
+    print(container.logs(since=now))
