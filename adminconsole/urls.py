e9ab28784f7273a50b87e65a8d64e214ca442411
pascalfree <pascal.free@ihdg.ch>
2025-06-22 19:18:52 +0200

    refactor view structure

--- adminconsole/urls.py
+++ adminconsole/urls.py
@@ -1,23 +1,10 @@
 """adminconsole URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-v    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from adminconsole import views
-from adminconsole import views_git
-from adminconsole import views_ca
+from adminconsole.views import git, create
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
@@ -25,7 +12,7 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('', views.home, name='home'),
     path('overview/<int:app_id>/', views.overview, name='overview'),
-    path('ca/import', views_ca.create_app, {'existing': True}, name='import'),
+    path('ca/import', create.create_app, {'existing': True}, name='import'),
     path('dom/form/<int:app_id>/', views.domain_form),
     path('mailtexts/<int:app_id>/', views.mailtexts),
     path('showlog/<int:app_id>/', views.show_log),
@@ -46,18 +33,17 @@ urlpatterns = [
     path('dumpdata/<int:app_id>/', views.dumpdata, name='dumpdata'),
 
 # create application urls in the right order how they flow
-    path('ca/start', views_ca.create_app),
-    path('github/request', views_git.github_request),
-    path('github/callback', views_git.github_callback),
-    path('ca/repo', views_git.select_repo),
-    path('ca/af', views_ca.app_form),
-    path('ca/clonerepo', views_git.clone_repo),
-    path('ck/form', views_ca.cookiecutter_form),
-    path('git/push', views_git.git_push),
-    path('ca/db', views_ca.init_db),
-    path('ca/ap', views_ca.admin_password),
-    path('ca/env/form', views_ca.env_form),
-    path('ca/env/dist', views_ca.env_dist2),
-    path('ca/docker', views_ca.docker2),
-
+    path('ca/start', create.create_app),
+    path('github/request', git.github_request),
+    path('github/callback', git.github_callback),
+    path('ca/repo', git.select_repo),
+    path('ca/af', create.app_form),
+    path('ca/clonerepo', git.clone_repo),
+    path('ck/form', create.cookiecutter_form),
+    path('git/push', git.git_push),
+    path('ca/db', create.init_db),
+    path('ca/ap', create.admin_password),
+    path('ca/env/form', create.env_form),
+    path('ca/env/dist', create.env_dist2),
+    path('ca/docker', create.docker2),
 ]
