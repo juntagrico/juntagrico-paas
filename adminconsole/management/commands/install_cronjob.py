c442607783832fd4f1489dae7805258ee6266564
pascalfree <pascal.free@ihdg.ch>
2026-02-16 18:14:57 +0100

    install cron tab correctly

--- adminconsole/management/commands/install_cronjob.py
+++ adminconsole/management/commands/install_cronjob.py
@@ -1,4 +1,4 @@
-import subprocess
+from crontab import CronTab
 from django.core.management.base import BaseCommand
 
 
@@ -9,12 +9,12 @@ class Command(BaseCommand):
     # entry point used by manage.py
     def handle(self, *args, **options):
         app_name = options['app_name'][0]
-        proc = subprocess.run(
-            [
-                '(crontab -l 2>/dev/null; '
-                'echo "59 23 * * * docker exec ' + app_name + ' python -m manage generate_depot_list") '
-                '| crontab -'
-            ],
-            stdout=subprocess.PIPE
-        )
-        print(str(proc.stdout))
+
+        new_command = 'docker exec ' + app_name + ' python -m manage generate_depot_list'
+        cron = CronTab(user='root')
+        job = cron.new(command=new_command)
+        job.minute.on(59)
+        job.hour.on(23)
+        cron.write()
+
+        print(new_command)
