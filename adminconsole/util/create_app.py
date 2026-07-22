8961880e737214ed5028b24679ab70fa4e57c685
pascalfree <pascal.free@ihdg.ch>
2026-07-22 18:20:36 +0200

    fix timezone for postgres 18

--- adminconsole/util/create_app.py
+++ adminconsole/util/create_app.py
@@ -46,7 +46,7 @@ def create_database(app_env, db_name, user_name, replace=False):
         cursor.execute("CREATE USER " + user_name + " WITH PASSWORD '" + password + "'")
         cursor.execute("ALTER ROLE " + user_name + " SET client_encoding TO 'utf8'")
         cursor.execute("ALTER ROLE " + user_name + " SET default_transaction_isolation TO 'read committed'")
-        cursor.execute("ALTER ROLE " + user_name + " SET timezone TO 'CET'")
+        cursor.execute("ALTER ROLE " + user_name + " SET timezone TO 'Europe/Zurich'")
         cursor.execute("GRANT ALL PRIVILEGES ON DATABASE " + db_name + " TO " + user_name)
     app_env.juntagrico_database_host = 'localhost'
     app_env.juntagrico_database_name = db_name
